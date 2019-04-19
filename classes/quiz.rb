3727a2a0bf64efad8f45a5ed7286bdbbb0bf90eb
Tdog16 <marshall.tynan16@gmail.com>
2019-04-20 01:57:21 +1000

    Finished defining Question class and run quiz method, added comments

--- classes/quiz.rb
+++ classes/quiz.rb
@@ -1,20 +1,35 @@
 class Question
-
+#Class to handle the prompted questions and their answers
     attr_accessor :prompt, :answer
     def initialize(prompt, answer)
-        @prompt = promt
+        @prompt = prompt
         @answer = answer
     end
+end
+#The questions and their multiple choice answers
+prompt1 = "How many strings does a standard guitar have?\n(a) 4\n(b) 6\n(c) 8\n(d) 7"
+prompt2 = "Master Chief is the protaganist in what game series?\n(a) Call of Duty\n(b) Halo\n(c) Borderlands\n(d) Left 4 Dead"
+prompt3 = "How many legs do spiders have?\n(a) 10\n(b) 6\n(c) 4\n(d) 8"
 
-    prompt1 = "How many strings does a standard guitar have?\n(a) 4\n(b) 6\n (c) 8\n (d) 7"
-    prompt2 = "Master Chief is the protaganist in what game series?\n(a) Call of Duty\n(b) Halo\n(c) Borderlands\n(d) Left 4 Dead"
-    prompt3 = "How many legs do spiders have?\n(a) 10\n(b) 6\n(c) 4\n(d) 8"
+#An array calling new questions with their answers
+questions = [
+    Question.new(prompt1, "b"),
+    Question.new(prompt2, "b"),
+    Question.new(prompt3, "d")
+]
+#defining a method to run the quiz with saved scoring
+def run_quiz(questions)
+    answer = ""
+    score = 0
+    for question in questions #a for loop for every question in the above array
+    puts question.prompt
+    answer = gets.chomp()
+    if answer == question.answer
+        score += 1
+    end
+end
+    puts "you got" + score.to_s + "/" + questions.length.to_s 
+end
 
 
-    questions = [
-        Question.new(prompt1, "b"),
-        Question.new(prompt2, "b"),
-        Question.new(prompt3, "8")
-
-    ]
 
